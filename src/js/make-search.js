07d09dd10df42d6a8750816fa9b1f14e0781f223
Marina Eremina <eremina.jobmail@gmail.com>
2019-04-11 19:40:46 +0200

    add search module

---
+++ src/js/make-search.js
@@ -0,0 +1,36 @@
+/* this module handles querying API with the provided keywords */
+
+import { renderSearchResults } from './render-results.js';
+
+//cache DOM, declare variables
+const form = document.querySelector('.js-form');
+const input = document.querySelector('.js-keyword-input');
+
+//bind events
+form.addEventListener('submit', makeSearch);
+
+//function declarations
+/* prevent default form submission and add custom form handling */
+function makeSearch(event) {
+  event.preventDefault();
+  const url = composeUrl();
+  sendRequest(url);
+}
+
+/* compose URL to query API with provided keywords */
+function composeUrl() {
+  const keywords = input.value;
+  const encodedKeywords = keywords.replace(/_/g, '%20');
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${encodedKeywords}`;
+  return url;
+}
+
+/* call the API */
+function sendRequest(url) {
+  fetch(url)
+    .then(response => response.json())
+    .then(searchData => {
+      renderSearchResults(searchData)
+    })
+    .catch(error => console.log(error));
+}
