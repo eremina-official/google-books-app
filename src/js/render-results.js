dc5f1909c4c8d256eca1d3b6206954a9d64d2deb
Marina Eremina <eremina.jobmail@gmail.com>
2019-04-11 19:41:27 +0200

    add render-results module

---
+++ src/js/render-results.js
@@ -0,0 +1,11 @@
+/* this module handles rendering search results to the screen */
+
+//cache DOM, declare variables
+const searchResults = document.querySelector('.search-results');
+
+//function declarations
+function renderSearchResults(searchData) {
+  console.log(searchData);
+}
+
+export { renderSearchResults };
